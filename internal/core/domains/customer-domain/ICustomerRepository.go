757b032aa54ec41e19473c76ea0f880bfbb4ac9c
Nana Emmanuel <dcrativ.dev@gmail.com>
2023-11-30 04:15:14 +0100

    ft - add customer postgres repository

---
+++ internal/core/domains/customer-domain/ICustomerRepository.go
@@ -0,0 +1,15 @@
+package customerDomain
+
+import (
+	"github.com/naelcodes/ab-backend/internal/core/dto"
+	"github.com/naelcodes/ab-backend/pkg/types"
+)
+
+type ICustomerRepository interface {
+	Count() (*int, error)
+	GetAll(*types.GetQueryParams) ([]*dto.GetCustomerDTO, error)
+	GetById(types.EID) (*dto.GetCustomerDTO, error)
+	Save(*CustomerAggregate) (*dto.GetCustomerDTO, error)
+	Update(*dto.UpdateCustomerDTO) error
+	Delete(types.EID) error
+}
--- internal/infrastructure/persistence/postgres/customer.repository.go
+++ internal/infrastructure/persistence/postgres/customer.repository.go
@@ -2,12 +2,15 @@ package postgres
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"slices"
 
 	"github.com/naelcodes/ab-backend/ent"
 	"github.com/naelcodes/ab-backend/ent/customer"
-	"github.com/naelcodes/ab-backend/internal/core/domain"
+	customerDomain "github.com/naelcodes/ab-backend/internal/core/domains/customer-domain"
 	"github.com/naelcodes/ab-backend/internal/core/dto"
+	CustomErrors "github.com/naelcodes/ab-backend/pkg/errors"
 	"github.com/naelcodes/ab-backend/pkg/types"
 )
 
@@ -19,7 +22,10 @@ type CustomerRepository struct {
 func (repo *CustomerRepository) GetById(id types.EID) (*dto.GetCustomerDTO, error) {
 	customer, err := repo.Database.Customer.Query().Where(customer.IDEQ(int(id))).First(repo.Context)
 	if err != nil {
-		return nil, err
+		if ent.IsNotFound(err) {
+			return nil, CustomErrors.RepositoryError(errors.New("customer record not found"))
+		}
+		return nil, CustomErrors.RepositoryError(fmt.Errorf("error getting customer record: %v", err))
 	}
 	customerDTO := CustomerModelToDTO(customer)
 	return customerDTO, nil
@@ -44,7 +50,7 @@ func (repo *CustomerRepository) GetAll(query *types.GetQueryParams) ([]*dto.GetC
 	customers, err := customerQuery.All(repo.Context)
 
 	if err != nil {
-		return nil, err
+		return nil, CustomErrors.RepositoryError(fmt.Errorf("error getting customers records: %v", err))
 	}
 
 	customerDTOList := CustomerModelListToDTOList(customers)
@@ -56,28 +62,70 @@ func (repo *CustomerRepository) Count() (*int, error) {
 	totalRowCount, err := repo.Database.Customer.Query().Where(customer.TagEQ(customer.Tag3)).Count(repo.Context)
 
 	if err != nil {
-		return nil, err
+		return nil, CustomErrors.RepositoryError(fmt.Errorf("error counting customers: %v", err))
 	}
 	return &totalRowCount, nil
 }
 
-func (repo *CustomerRepository) GetCustomerOpenPayments(id types.EID) (*dto.GetCustomerOpenPaymentsDTO, error) {
-	// TODO
-	return nil, nil
-}
+func (repo *CustomerRepository) Save(customer *customerDomain.CustomerAggregate) (*dto.GetCustomerDTO, error) {
 
-func (repo *CustomerRepository) GetCustomerUnPaidInvoices(id types.EID) (*dto.GetCustomerUnpaidInvoicesDTO, error) {
-	// TODO
-	return nil, nil
-}
+	customerRecord, err := repo.Database.Customer.Create().
+		SetCustomerName(customer.CustomerName).
+		SetAbKey(customer.AbKey).
+		SetAlias(customer.Alias).
+		SetTmcClientNumber(customer.TmcClientNumber).
+		SetAccountNumber(customer.AccountNumber).
+		SetState(customer.State).
+		Save(repo.Context)
+	if err != nil {
+		return nil, CustomErrors.RepositoryError(fmt.Errorf("error saving customer record: %v", err))
+	}
 
-func (repo *CustomerRepository) Save(customer *domain.CustomerAggregate) error {
-
-	return nil
+	customerDTO := CustomerModelToDTO(customerRecord)
+	return customerDTO, nil
 }
-func (repo *CustomerRepository) Update(customer *domain.CustomerAggregate) error {
+func (repo *CustomerRepository) Update(customer *dto.UpdateCustomerDTO) error {
+
+	UpdateQueryBuilder := repo.Database.Customer.UpdateOneID(int(customer.Id))
+
+	if customer.Customer_name != nil {
+		UpdateQueryBuilder.SetCustomerName(*customer.Customer_name)
+	}
+
+	if customer.Alias != nil {
+		UpdateQueryBuilder.SetAlias(*customer.Alias)
+	}
+
+	if customer.Tmc_client_number != nil {
+		UpdateQueryBuilder.SetTmcClientNumber(*customer.Tmc_client_number)
+	}
+
+	if customer.Account_number != nil {
+		UpdateQueryBuilder.SetAccountNumber(*customer.Account_number)
+	}
+
+	if customer.State != nil {
+		UpdateQueryBuilder.SetState(*customer.State)
+	}
+
+	_, err := UpdateQueryBuilder.Save(repo.Context)
+
+	if err != nil {
+		return CustomErrors.RepositoryError(fmt.Errorf("error updating customer record: %v", err))
+	}
+
 	return nil
 }
 func (repo *CustomerRepository) Delete(id types.EID) error {
+
+	deletedCount, err := repo.Database.Customer.Delete().Where(customer.IDEQ(int(id))).Exec(repo.Context)
+
+	if err != nil {
+		return CustomErrors.RepositoryError(fmt.Errorf("error deleting customer record: %v", err))
+	}
+
+	if deletedCount == 0 {
+		return CustomErrors.RepositoryError(errors.New("customer record not found"))
+	}
 	return nil
 }
