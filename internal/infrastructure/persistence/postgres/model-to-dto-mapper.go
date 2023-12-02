94c4cf952bc87f51262768898368d0156ca26329
Nana Emmanuel <dcrativ.dev@gmail.com>
2023-12-02 01:45:01 +0100

    ft(dto) - Add Payment model to dto mapper

--- internal/infrastructure/persistence/postgres/model-to-dto-mapper.go
+++ internal/infrastructure/persistence/postgres/model-to-dto-mapper.go
@@ -28,3 +28,34 @@ func CustomerModelListToDTOList(customers []*ent.Customer) []*dto.GetCustomerDTO
 
 	return customerDTOList
 }
+
+func PaymentModelToDTO(payment *ent.Payment, embedCustomer bool) *dto.GetPaymentDTO {
+	paymentDTO := new(dto.GetPaymentDTO)
+	paymentDTO.Id = payment.ID
+	paymentDTO.Amount = payment.Amount
+	paymentDTO.PaymentNumber = payment.Number
+	paymentDTO.PaymentDate = payment.Date
+	paymentDTO.PaymentMode = string(payment.Fop)
+	paymentDTO.Balance = payment.Balance
+	paymentDTO.UsedAmount = payment.UsedAmount
+	paymentDTO.Status = string(payment.Status)
+
+	if embedCustomer {
+		paymentDTO.IdCUstomer = nil
+		paymentDTO.Customer = CustomerModelToDTO(payment.Edges.Customer)
+	} else {
+		paymentDTO.IdCUstomer = &payment.Edges.Customer.ID
+	}
+
+	return paymentDTO
+}
+
+func PaymentModelListToDTOList(payments []*ent.Payment, embedCustomer bool) []*dto.GetPaymentDTO {
+	paymentDTOList := make([]*dto.GetPaymentDTO, 0)
+
+	for _, payment := range payments {
+		paymentDTOList = append(paymentDTOList, PaymentModelToDTO(payment, embedCustomer))
+	}
+
+	return paymentDTOList
+}
